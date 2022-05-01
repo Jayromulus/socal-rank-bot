70ec07a04bc161000f6cd03c2a122090447c1ac4
Jayromulus <jerbear2@gmail.com>
2022-05-01 10:55:06 -0400

    a lil tidy up

--- db.js
+++ db.js
@@ -3,15 +3,16 @@ const Sequelize = require('sequelize')
 const db = new Sequelize(process.env.DATABASE_URL
   // uncomment below for deployment or using remote db
   , {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
       ssl: {
-          require: true,
-          rejectUnauthorized: false
+        require: true,
+        rejectUnauthorized: false
       }
+    }
   }
-}
 );
 
 module.exports = {
--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const guild_members = [
   'Bart',
   'Kiomi',
 ].sort();
-// console.log('member runs:', memberRuns);
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
 const models = require('./models');
 const { Op } = require('sequelize');
@@ -43,7 +42,7 @@ let playerRunsGeneral;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity('Running some test, hopefully.');
+  client.user.setActivity('!help for commands');
 })
 
 client.on('messageReactionAdd', async (reaction, user) => {
@@ -66,158 +65,158 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 
 client.on('messageCreate', async (message) => {
-  // console.log(message);
   if (message.author.bot) return;
   if (message.channelId === process.env.CHANNEL_1 || message.channelId === process.env.CHANNEL_2) {
     if (message.content.indexOf(process.env.PREFIX) !== 0) return;
     const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     let playerInfo;
-    // if (command === 'ping') {
-    //   message.channel.send('Pong.');
-    // }
     if (command === 'rank') {
       playerRunsGeneral = await models.Team.findAll({ where: { member: { [Op.iLike]: `%${args[0]}%` } } })
-      
-      // console.log('playerRuns',playerRunsGeneral, args[0]);
+
 
       switch (args.length) {
         case 0:
           message.channel.send('No player input');
           break;
         case 1:
-          // find all things accorgind to player (each month and score)
-          playerInfo = await models.Rank.findAll({ where: { player: { [Op.iLike]: `%${args[0]}%` } }, order: [['createdAt', 'DESC']] })
-          // console.log(playerInfo);
-          if (playerInfo[0]) {
-            const exampleEmbed = newEmbed(playerInfo[index], findDamage(playerRunsGeneral, playerInfo[index].month), playerInfo.length)
+          try {
+            playerInfo = await models.Rank.findAll({ where: { player: { [Op.iLike]: `%${args[0]}%` } }, order: [['createdAt', 'DESC']] })
+            if (playerInfo[0]) {
+              const exampleEmbed = newEmbed(playerInfo[index], findDamage(playerRunsGeneral, playerInfo[index].month), playerInfo.length)
 
-            // const msg = await message.channel.send({ embeds: [exampleEmbed] })
-            const backId = 'back'
-            const forwardId = 'forward'
-            const backButton = new MessageButton({
-              style: 'SECONDARY',
-              label: 'Newer',
-              emoji: '⬅️',
-              customId: backId
-            })
-            const forwardButton = new MessageButton({
-              style: 'SECONDARY',
-              label: 'Older',
-              emoji: '➡️',
-              customId: forwardId
-            })
-
-            const msg = await message.channel.send({
-              embeds: [exampleEmbed],
-              components: playerInfo.length === 1
-                ? []
-                : [new MessageActionRow({ components: [forwardButton] })]
-            })
-
-            if (playerInfo.length === 0) return
-
-            // Collect button interactions (when a user clicks a button),
-            // but only when the button as clicked by the original message author
-            const collector = msg.createMessageComponentCollector({
-              filter: ({ user }) => true
-            })
-
-            let currentIndex = index
-            collector.on('collect', async interaction => {
-              // Increase/decrease index
-              interaction.customId === backId ? (currentIndex -= 1) : (currentIndex += 1)
-              // Respond to interaction by updating message with new embed
-              await interaction.update({
-                embeds: [newEmbed(playerInfo[currentIndex], findDamage(playerRunsGeneral, playerInfo[currentIndex].month), playerInfo.length)],
-                components: [
-                  new MessageActionRow({
-                    components: [
-                      // back button if it isn't the start
-                      ...(currentIndex ? [backButton] : []),
-                      // forward button if it isn't the end
-                      ...(currentIndex + 1 < playerInfo.length ? [forwardButton] : [])
-                    ]
-                  })
-                ]
+              const backId = 'back'
+              const forwardId = 'forward'
+              const backButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Newer',
+                emoji: '⬅️',
+                customId: backId
               })
-            })
+              const forwardButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Older',
+                emoji: '➡️',
+                customId: forwardId
+              })
+
+              const msg = await message.channel.send({
+                embeds: [exampleEmbed],
+                components: playerInfo.length === 1
+                  ? []
+                  : [new MessageActionRow({ components: [forwardButton] })]
+              })
+
+              if (playerInfo.length === 0) return
+
+              // Collect button interactions (when a user clicks a button),
+              // but only when the button as clicked by the original message author
+              const collector = msg.createMessageComponentCollector({
+                filter: ({ user }) => true
+              })
+
+              let currentIndex = index
+              collector.on('collect', async interaction => {
+                // Increase/decrease index
+                interaction.customId === backId ? (currentIndex -= 1) : (currentIndex += 1)
+                // Respond to interaction by updating message with new embed
+                await interaction.update({
+                  embeds: [newEmbed(playerInfo[currentIndex], findDamage(playerRunsGeneral, playerInfo[currentIndex].month), playerInfo.length)],
+                  components: [
+                    new MessageActionRow({
+                      components: [
+                        // back button if it isn't the start
+                        ...(currentIndex ? [backButton] : []),
+                        // forward button if it isn't the end
+                        ...(currentIndex + 1 < playerInfo.length ? [forwardButton] : [])
+                      ]
+                    })
+                  ]
+                })
+              })
+            }
+          }
+          catch (e) {
+            console.log('error at 149:', e);
           }
           break;
         case 2:
-          // find rank of player in year
-          playerInfo = await models.Rank.findAll({ where: { player: { [Op.iLike]: `%${args[0]}%` }, year: args[1] }, order: [['createdAt', 'DESC']] })
-          if (playerInfo[0]) {
-            const exampleEmbed = newEmbed(playerInfo[index],findDamage(playerRunsGeneral, playerInfo[index].month), playerInfo.length)
+          try {
+            // find rank of player in year
+            playerInfo = await models.Rank.findAll({ where: { player: { [Op.iLike]: `%${args[0]}%` }, year: args[1] }, order: [['createdAt', 'DESC']] })
+            if (playerInfo[0]) {
+              const exampleEmbed = newEmbed(playerInfo[index], findDamage(playerRunsGeneral, playerInfo[index].month), playerInfo.length)
 
 
-            const backId = 'back'
-            const forwardId = 'forward'
-            const backButton = new MessageButton({
-              style: 'SECONDARY',
-              label: 'Newer',
-              emoji: '⬅️',
-              customId: backId
-            })
-            const forwardButton = new MessageButton({
-              style: 'SECONDARY',
-              label: 'Older',
-              emoji: '➡️',
-              customId: forwardId
-            })
-
-            const msg = await message.channel.send({
-              embeds: [exampleEmbed],
-              components: playerInfo.length === 1
-                ? []
-                : [new MessageActionRow({ components: [forwardButton] })]
-            })
-
-            if (playerInfo.length === 0) return
-
-            // Collect button interactions (when a user clicks a button),
-            // but only when the button as clicked by the original message author
-            const collector = msg.createMessageComponentCollector({
-              filter: ({ user }) => true
-            })
-
-            let currentIndex = index
-            collector.on('collect', async interaction => {
-              // Increase/decrease index
-              interaction.customId === backId ? (currentIndex -= 1) : (currentIndex += 1)
-              // Respond to interaction by updating message with new embed
-              await interaction.update({
-                embeds: [newEmbed(playerInfo[currentIndex],findDamage(playerRunsGeneral, playerInfo[currentIndex].month), playerInfo.length)],
-                components: [
-                  new MessageActionRow({
-                    components: [
-                      // back button if it isn't the start
-                      ...(currentIndex ? [backButton] : []),
-                      // forward button if it isn't the end
-                      ...(currentIndex + 1 < playerInfo.length ? [forwardButton] : [])
-                    ]
-                  })
-                ]
+              const backId = 'back'
+              const forwardId = 'forward'
+              const backButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Newer',
+                emoji: '⬅️',
+                customId: backId
               })
-            })
-            // msg.react('◀');
-            // msg.react('▶');
+              const forwardButton = new MessageButton({
+                style: 'SECONDARY',
+                label: 'Older',
+                emoji: '➡️',
+                customId: forwardId
+              })
+
+              const msg = await message.channel.send({
+                embeds: [exampleEmbed],
+                components: playerInfo.length === 1
+                  ? []
+                  : [new MessageActionRow({ components: [forwardButton] })]
+              })
+
+              if (playerInfo.length === 0) return
+
+              // Collect button interactions (when a user clicks a button),
+              // but only when the button as clicked by the original message author
+              const collector = msg.createMessageComponentCollector({
+                filter: ({ user }) => true
+              })
+
+              let currentIndex = index
+              collector.on('collect', async interaction => {
+                // Increase/decrease index
+                interaction.customId === backId ? (currentIndex -= 1) : (currentIndex += 1)
+                // Respond to interaction by updating message with new embed
+                await interaction.update({
+                  embeds: [newEmbed(playerInfo[currentIndex], findDamage(playerRunsGeneral, playerInfo[currentIndex].month), playerInfo.length)],
+                  components: [
+                    new MessageActionRow({
+                      components: [
+                        // back button if it isn't the start
+                        ...(currentIndex ? [backButton] : []),
+                        // forward button if it isn't the end
+                        ...(currentIndex + 1 < playerInfo.length ? [forwardButton] : [])
+                      ]
+                    })
+                  ]
+                })
+              })
+            }
+          } catch (e) {
+            console.log('error at 213:', e);
           }
           break;
         case 3:
-          // find rank of player in month and year
-          // console.log(args);
-          playerInfo = await models.Rank.findOne({ where: { player: { [Op.iLike]: `%${args[0]}%` }, month: { [Op.iLike]: `%${args[1]}%` }, year: args[2] } })
-          // console.log(playerInfo);
-          const exampleEmbed = newEmbed(playerInfo,findDamage(playerRunsGeneral, playerInfo.month), 1)
-          const msg = await message.channel.send({ embeds: [exampleEmbed] })
+          try {
+            playerInfo = await models.Rank.findOne({ where: { player: { [Op.iLike]: `%${args[0]}%` }, month: { [Op.iLike]: `%${args[1]}%` }, year: args[2] } })
+            const exampleEmbed = newEmbed(playerInfo, findDamage(playerRunsGeneral, playerInfo.month), 1)
+            const msg = await message.channel.send({ embeds: [exampleEmbed] })
+          } catch (e) {
+            console.log('error at 225:', e);
+          }
           break;
         default:
           message.channel.send('Invalid input. Type `!help` for help on usage.');
       }
     }
     if (command === 'runs') {
-// REWORK THIS TO HAVE NO ARGUMENTS FOR CURRENT DAY RUNS AND A MONTH YEAR TO GET SPECIFIC CB RUNS (Op.iLike %month% AND Op.includes? year)
+      // REWORK THIS TO HAVE NO ARGUMENTS FOR CURRENT DAY RUNS AND A MONTH YEAR TO GET SPECIFIC CB RUNS (Op.iLike %month% AND Op.includes? year)
 
       // const TODAY_START = new Date().setHours(0, 0, 0, 0);
       // const NOW = new Date().setHours(23.59, 59, 999);
@@ -321,71 +320,37 @@ function newEmbed(info, damage, length) {
   return new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setTitle(info.player)
-    // .setURL('https://discord.js.org/')
-    // .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
-    // .setDescription('Some description here')
-    // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
     .addFields(
-      // { name: 'Regular field title', value: 'Some value here' },
-      // { name: '\u200B', value: '\u200B' },
       { name: 'CB', value: `${info.month} ${info.year}`, inline: true },
       { name: 'Rank', value: `${info.rank}`, inline: true },
-      { name: 'Score', value: `${info.score}`, inline: true },
+      { name: 'Score', value: `${info.score.toLocaleString('en-us')}`, inline: true },
       { name: '\u200B', value: `\u200B`, inline: true },
       { name: '\u200B', value: `\u200B`, inline: true },
-      { name: 'Damage', value: `${damage !== 0 ? damage : '*unavailable*'}`, inline: true }
+      { name: 'Damage', value: `${damage !== 0 ? damage.toLocaleString('en-us') : '*unavailable*'}`, inline: true }
     )
-    // .addField('Inline field title', 'Some value here', true)
-    // .setImage('https://i.imgur.com/AfFp7pu.png')
     .setTimestamp()
     .setFooter('Use the buttons to scroll the results');
 }
 
 function runEmbed(totals, start) {
   let displayFields = [];
-  // console.log(totals, start);
   for (let i = start; i < (start + 15); i++) {
-    // console.log('loop:', totals);
     if (totals[i] !== undefined)
       displayFields.push(totals[i])
   }
-  // console.log(displayFields);
   return new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setTitle('Member Runs')
-    // .setURL('https://discord.js.org/')
-    // .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
-    // .setDescription('Some description here')
-    // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
     .addFields(
-      // { name: 'Regular field title', value: 'Some value here' },
-      // { name: '\u200B', value: '\u200B' },
       displayFields
     )
-    // .addField('Inline field title', 'Some value here', true)
-    // .setImage('https://i.imgur.com/AfFp7pu.png')
     .setTimestamp()
-  // .setFooter('Use the emoted to filter the results');
 }
 
 function findDamage(runs, month) {
-  // console.log(runs[runs.length-1].cb.includes(month), runs[0].damage);
-  // console.log('Damage:',runs.filter(r => r.cb.includes(month)).reduce((a,b) => {a?.damage + b?.damage}, 0));
-  let temp = runs.filter(r => {console.log(r.cb.includes(month)); return r.cb.includes(month)})
-  // console.log('runs',runs.length);
-  // console.log('temp',temp.length);
-  // console.log(temp[0]?.damage);
-  // console.log(temp[0]);
-  let test = temp.reduce((a,b) => a + b.damage, 0)
-  // console.log(test);
+  let temp = runs.filter(r => { return r.cb.includes(month) })
+  let test = temp.reduce((a, b) => a + b.damage, 0)
   return test
 }
-
-// client.on('messageReactionAdd', async (react_orig, user) => {
-//   if(react_orig.message.author.id === user.id) return;
-//   // when the app loads, make it so that the current index is the last spot on the array, forward resets to the first spot
-//   // react_orig.message.channel.send('if this going the right place?')
-//   console.log(react_orig);
-// })
 
 client.login(process.env.TOKEN);
